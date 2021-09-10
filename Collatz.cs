b983fdc7ee22a79d01d782180d8c6a8acf0e6b45
XochtilHEIlZMakerBymasFanboy <89978848+XochtilHEIlZMakerBymasFanboy@users.noreply.github.com>
2021-09-10 06:30:24 +0200

    First checkin

---
+++ Collatz.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+
+namespace Programmering_2_Första_lektion
+{
+    public class Collatz
+    {
+        bool foundTwo = false;
+        bool foundFour = false;
+
+        public void Calculate(BigInteger value)
+        {
+
+            BigInteger calculatedValue;
+            if (IsEven(value))
+            {
+                calculatedValue = value / 2;
+
+            }
+            else
+            {
+                calculatedValue = value * 3 + 1;
+            }
+            PrintNum(calculatedValue);
+            if (CheckInfinityLoop(calculatedValue))
+            {
+                return;
+
+            }
+            Calculate(calculatedValue);
+        }
+
+        private bool IsEven(BigInteger inputValue)
+        {
+            if (inputValue % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void PrintNum(BigInteger inputValue)
+        {
+            Console.WriteLine(inputValue);
+        }
+
+        private bool CheckInfinityLoop(BigInteger inputValue) 
+        {
+
+            if (!foundFour && inputValue == 4)
+            {
+                foundFour = true;
+                return false;
+            }           
+            else if (foundFour && !foundTwo && inputValue == 2)
+            {
+                foundTwo = true;
+                return false;
+            }
+            else if (foundFour && foundTwo && inputValue == 1) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+namespace Programmering_2_Första_lektion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test the Collatz conjecture!");
+            Console.WriteLine("if you wish to find out more about the Collatz conjectue then press ?");
+            string inputValue = Console.ReadLine();
+            if (inputValue[0] == '?')
+            {
+                Console.WriteLine("The Collatz conjectue claims that any number no matter how big will if an even number is divided by 2 and the odd numbers " +
+                    "are multiplied by 3 and gets a 1 added to it the number will eventually reach the value of 1, were it wil then be stuck in a loop.");
+            }
+            else 
+            { 
+                if (BigInteger.TryParse(inputValue, out BigInteger BResult) && BResult > 0)
+                {
+                    Collatz c = new Collatz();
+                    c.Calculate(BResult);
+                    Console.WriteLine("You've reached the infinite loop");
+                }
+
+                else
+                {
+                    Console.WriteLine("Input not valid as an integer value bigger than zero. Try again");
+                }
+            }
+            Console.ReadKey();
+        }   
+    }
+}
